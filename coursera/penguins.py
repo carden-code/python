992d3e40dd4c51f37d02b51209e57d43a808a40f
Carden <carden.ruby@gmail.com>
2020-10-23 19:07:34 +0300

    complited

---
+++ coursera/penguins.py
@@ -0,0 +1,11 @@
+num = int(input())
+a = '   _~_    '
+b = '  (o o)   '
+c = ' /  V  \  '
+d = '/(  _  )\ '
+i = '  ^^ ^^   '
+print(a * num)
+print(b * num)
+print(c * num)
+print(d * num)
+print(i * num)
