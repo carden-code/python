2f7c68f9dd0110732b119a1647de8ad05c4bde0d
Carden <carden.ruby@gmail.com>
2020-10-26 19:51:29 +0300

    complite

---
+++ coursera/next_and_previous.py
@@ -0,0 +1,5 @@
+number = int(input())
+next_num = number + 1
+previous_num = number - 1
+print('The next number for the number', number, 'is', next_num, end='.\n')
+print('The previous number for the number', number, 'is', previous_num, end='.')
