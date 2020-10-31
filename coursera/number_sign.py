8d0aeef73135f5f0d0cbc17df45ea7f4cafcb40b
Carden <carden.ruby@gmail.com>
2020-10-31 20:09:37 +0300

    modification comment

--- coursera/number_sign.py
+++ coursera/number_sign.py
@@ -1,3 +1,20 @@
+# In mathematics, the function sign (x) (number sign) is defined like this:
+#
+# sign (x) = 1 if x> 0,
+#
+# sign (x) = - 1 if x <0,
+#
+# sign (x) = 0 if x = 0.
+#
+# For a given number x, output the value sign (x).
+#
+# Input format
+#
+# One integer is entered.
+#
+# Output format
+#
+# Print the answer to the problem.
 x = int(input())
 if x > 0:
     print(1)
