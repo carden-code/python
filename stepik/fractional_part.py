7a2c1973d03ca8a82c6426ebefb2a8d09265dd0a
Carden <carden.ruby@gmail.com>
2020-11-01 21:44:05 +0300

    complited

---
+++ stepik/fractional_part.py
@@ -0,0 +1,10 @@
+# A positive real number is given. Output its fractional part.
+#
+# Input data format
+# The input to the program is a positive real number.
+#
+# Output data format
+# The program should output the fractional part of the number in accordance with the condition of the problem.
+num = float(input())
+fractional = num - (int(num))
+print(fractional)
