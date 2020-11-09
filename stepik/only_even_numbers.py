7d7e43727a4e22cd48d5290e970e41c9e8d2b000
Carden <carden.ruby@gmail.com>
2020-11-09 21:29:49 +0300

    comments

--- stepik/only_even_numbers.py
+++ stepik/only_even_numbers.py
@@ -1,3 +1,10 @@
+# Write a program that reads a sequence of 10 integers and determines whether each of them is even or not.
+#
+# Input data format
+# The program receives 10 integers as input, each on a separate line.
+#
+# Output data format
+# The program should print the string "YES" if all numbers are even and "NO" otherwise.
 even = 0
 odd = 0
 for _ in range(10):
