b9731882035cf4551a2d762fcc2a05473c7eb452
Carden <carden.ruby@gmail.com>
2020-11-10 19:52:39 +0300

    while 2

---
+++ stepik/to_end_2.py
@@ -0,0 +1,13 @@
+# The program receives a sequence of words as input, each word on a separate line.
+# The end of the sequence is the word "END" or "end" (in capital or small letters, without quotes).
+# Write a program that prints out the members of a given sequence.
+#
+# Input data format
+# The program receives a sequence of words as input, each word on a separate line.
+#
+# Output data format
+# The program should output the members of the given sequence.
+text = input()
+while text != 'END' and text != 'end':
+    print(text)
+    text = input()
