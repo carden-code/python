9c24fc8e5ad009433d8acf1b8f7d8f43f4765c9a
Carden <carden.ruby@gmail.com>
2020-11-20 22:08:42 +0300

    complited

---
+++ stepik/third_digit.py
@@ -0,0 +1,10 @@
+# A natural number n is given, (n> 99). Write a program that determines its third (from the beginning) digit.
+#
+# Input data format
+# The input to the program is one natural number, consisting of at least three digits.
+#
+# Output data format
+# The program should display its third (from the beginning) digit.
+num = input()
+digit = int(num[2])
+print(digit)
