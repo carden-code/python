fbcf2e125ccb3865b6ec87053d07ca7b93bb604a
Carden <carden.ruby@gmail.com>
2020-11-24 22:02:02 +0300

    complited

---
+++ stepik/sWAP_cASE.py
@@ -0,0 +1,10 @@
+# The input to the program is a string.
+# Write a program that changes the case of characters, in other words,
+# replace all lowercase characters with uppercase ones and vice versa.
+#
+# Input data format
+# The input to the program is a string.
+#
+# Output data format
+# The program should output a string in accordance with the condition of the problem.
+print(input().swapcase())
