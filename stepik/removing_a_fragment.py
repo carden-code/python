e700e42909561af4e480c2d63f990b076d3f063d
Carden <carden.ruby@gmail.com>
2020-11-26 11:42:34 +0300

    complited

---
+++ stepik/removing_a_fragment.py
@@ -0,0 +1,11 @@
+# The input to the program is a line of text in which the letter "h" occurs at least twice.
+# Write a program that removes the first and
+# last occurrences of the letter "h" from this string, as well as any characters in between.
+#
+# Input data format
+# The input to the program is a line of text.
+#
+# Output data format
+# The program should display the text in accordance with the condition of the problem.
+string = input()
+print(string[:string.find('h')] + string[string.rfind('h') + 1:])
