1b432fd1b1eca5ec6ac5c80a127d5e6e23200d6d
Carden <carden.ruby@gmail.com>
2020-11-30 20:35:37 +0300

    complited

---
+++ stepik/remove_me_completely.py
@@ -0,0 +1,9 @@
+# The input to the program is a line of text. Write a program that removes all occurrences of the @ symbol.
+#
+# Input data format
+# The input to the program is a line of text.
+#
+# Output data format
+# The program should display the text in accordance with the condition of the problem.
+string = input()
+print(string.replace('@', ''))
