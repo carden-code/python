cb0360a3882a23d7643c12a55b95a136b3b6ac4e
Carden <carden.ruby@gmail.com>
2020-11-30 20:33:29 +0300

    complited

---
+++ stepik/replace_me_completely.py
@@ -0,0 +1,10 @@
+# The input to the program is a line of text.
+# Write a program that replaces all occurrences of the number 1 with the word "one".
+#
+# Input data format
+# The input to the program is a line of text.
+#
+# Output data format
+# The program should display the text in accordance with the condition of the problem.
+string = input()
+print(string.replace('1', 'one'))
