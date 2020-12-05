37944f213293b2e17d13691e6c30649f67b4f81c
Carden <carden.ruby@gmail.com>
2020-12-05 23:05:55 +0300

    complited

---
+++ stepik/one_line_3.py
@@ -0,0 +1,10 @@
+# The input to the program is a string of text containing integers. Write a program using a list expression that prints squares of even numbers that do not end in 4.
+#
+# Input data format
+# The input to the program is a string of text containing integers separated by a space character.
+#
+# Output data format
+# The program should display the text in accordance with the condition of the problem.
+#
+# Note. The program can be written in one line of code.
+print(*[int(i) ** 2 for i in input().split() if int(i) % 2 == 0 and int(i) ** 2 % 10 != 4])
