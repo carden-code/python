e5688c4dd0a16403402d0c696a2c9b752cb3fce5
Carden <carden.ruby@gmail.com>
2020-12-21 22:10:26 +0300

    not complited

--- stepik/ave_caesar.py
+++ stepik/ave_caesar.py
@@ -1,42 +1,37 @@
 string = 'Day, mice. "Year" is a mistake!'
 english_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+max_index = (len(english_alphabet) - 1)
 list_text = [c for c in string.split()]
-list_not_sym = []
-b = ''
-for s in list_text:
-    for j in s:
-        if j.isalpha():
-            b += j
-    list_not_sym.append(b)
-    b = ''
-cs = ''
+new = ''
 print(list_text)
-print(list_not_sym)
-for c in list_text:
-    list_c = c
-    step = len(list_not_sym[list_text.index(c)])
-    for j in list_c:
-        if j.lower() in english_alphabet:
-            index = english_alphabet.index(j.lower())
-            index_step = index + step
-            if j == j.upper():
-                if index_step > len(english_alphabet) - 1:
-                    index = index_step - (len(english_alphabet) - 1)
-                    cs += english_alphabet[index + step].upper()
-                elif index_step == len(english_alphabet) - 1:
-                    index = index_step - (len(english_alphabet) - 1)
-                    cs += english_alphabet[index + step].upper()
+for s in list_text:
+    index = list_text.index(s)
+    count = 0
+    step = count
+    for i in s:
+        if i.isalpha():
+            count += 1
+    for c in s:
+        new = ''
+        if c.isalpha():
+            index_alpha = english_alphabet.index(c.lower())
+            index_step = index_alpha + step
+            if c == c.upper():
+                if index_step >= max_index:
+                    index_step = ((max_index - index_alpha) - step) - 1
+                    new += english_alphabet[index_step].upper()
                 else:
-                    cs += english_alphabet[index + step].upper()
-            elif j == j.lower():
-                if index_step > len(english_alphabet) - 1:
-                    index = index_step - (len(english_alphabet) - 1)
-                    cs += english_alphabet[index + step]
-                elif index_step == len(english_alphabet):
-                    index = index_step - (len(english_alphabet) - 1)
-                    cs += english_alphabet[index + step]
+                    new += english_alphabet[index_step].upper()
+            if c == c.lower():
+                if index_step >= max_index:
+                    index_step = ((max_index - index_alpha) - step) - 1
+                    new += english_alphabet[index_step]
                 else:
-                    cs += english_alphabet[index + step]
-            else:
-                cs += j
-print(cs)
+                    new += english_alphabet[index_step]
+        else:
+            new += c
+        list_text[index] = new
+    new = ''
+
+
+print(list_text)
