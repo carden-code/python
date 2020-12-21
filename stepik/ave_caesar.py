e238514961217f68bba57451865ea7ca1521dacd
Carden <carden.ruby@gmail.com>
2020-12-21 22:41:56 +0300

    complited

--- stepik/ave_caesar.py
+++ stepik/ave_caesar.py
@@ -1,37 +1,46 @@
-string = 'Day, mice. "Year" is a mistake!'
+# At the entrance to the program, a line of text in English, in which you need to encrypt all the words.
+# Each word in the string should be encrypted using the Caesar cipher (cyclic shift by the length of that word).
+# Lowercase letters remain lowercase and uppercase letters remain uppercase.
+#
+# Input data format
+# The input to the program is a line of text in English.
+#
+# Output data format
+# The program should output the cipher text in accordance with the condition of the problem.
+#
+# Note. Non-English characters are not modified.
+string = input()
 english_alphabet = 'abcdefghijklmnopqrstuvwxyz'
 max_index = (len(english_alphabet) - 1)
 list_text = [c for c in string.split()]
-new = ''
-print(list_text)
+encrypted_string = ''
+step = 0
+
 for s in list_text:
     index = list_text.index(s)
-    count = 0
-    step = count
     for i in s:
         if i.isalpha():
-            count += 1
+            step += 1
     for c in s:
-        new = ''
         if c.isalpha():
             index_alpha = english_alphabet.index(c.lower())
             index_step = index_alpha + step
-            if c == c.upper():
+            if c.isupper():
                 if index_step >= max_index:
-                    index_step = ((max_index - index_alpha) - step) - 1
-                    new += english_alphabet[index_step].upper()
+                    index_step = (step - (max_index - index_alpha)) - 1
+                    encrypted_string += english_alphabet[index_step].upper()
                 else:
-                    new += english_alphabet[index_step].upper()
-            if c == c.lower():
+                    encrypted_string += english_alphabet[index_step].upper()
+            if c.islower():
                 if index_step >= max_index:
-                    index_step = ((max_index - index_alpha) - step) - 1
-                    new += english_alphabet[index_step]
+                    index_step = (step - (max_index - index_alpha)) - 1
+                    encrypted_string += english_alphabet[index_step]
                 else:
-                    new += english_alphabet[index_step]
+                    encrypted_string += english_alphabet[index_step]
         else:
-            new += c
-        list_text[index] = new
-    new = ''
+            encrypted_string += c
+    list_text[index] = encrypted_string
+    encrypted_string = ''
+    step = 0
 
-
-print(list_text)
+print(' '.join(list_text))
