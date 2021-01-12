5ddaa5e2fe429343472acbe898a34b04f852ad31
Carden <carden.ruby@gmail.com>
2021-01-12 21:48:51 +0300

    refactor

--- stepik/OOP_python/project_railway/station.py
+++ stepik/OOP_python/project_railway/station.py
@@ -6,22 +6,14 @@
 # Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
 class Station:
     def __init__(self, name):
-        self.__name = name
-        self.__trains = []
+        self.name = name
+        self.trains = []
 
     def arrive(self, train):
         self.trains.append(train)
-
-    @property
-    def name(self):
-        return self.__name
-
-    @property
-    def trains(self):
-        return self.__trains
 
     def type_train(self, type):
         pass
 
     def send_train(self, train):
-        self.__trains.remove(train)
+        self.trains.remove(train)
