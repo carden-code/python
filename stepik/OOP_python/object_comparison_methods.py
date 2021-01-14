1a2f2241aae8e50a02214062abb16148375294f9
Carden <carden.ruby@gmail.com>
2021-01-14 21:25:35 +0300

    eq_lt_ne_le

---
+++ stepik/OOP_python/object_comparison_methods.py
@@ -0,0 +1,25 @@
+# Специальные методы сравнения объектов классов.
+class Rectangle:
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    @property
+    def area(self):
+        return self.a * self.b
+
+    def __eq__(self, other):
+        print('__eq__ call')
+        if isinstance(other, Rectangle):
+            return self.a == other.a and self.b == other.b
+
+    def __lt__(self, other):
+        print('__lt__ call')
+        if isinstance(other, Rectangle):
+            return self.area < other.area
+        elif isinstance(other, (int, float)):
+            return self.area < other
+
+    def __le__(self, other):
+        return self == other or self < other
