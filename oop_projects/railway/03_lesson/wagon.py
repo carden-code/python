9d3e4b57f2b798dd90fc1a57f635dc10248b3d27
Carden <carden.ruby@gmail.com>
2021-02-05 16:49:32 +0300

    add new task

---
+++ oop_projects/railway/03_lesson/wagon.py
@@ -0,0 +1,16 @@
+# Класс Wagon (Вагон):
+#  - Имеет тип(passenger/cargo), эти данные указываются при создании экземпляра класса.
+#  - Может возвращать тип вагона.
+class Wagon:
+    #  Иницилизация объекта. Создаёт атрибуты объекта.
+    def __init__(self, wagon_type):
+        self.__wagon_type = wagon_type
+
+    # Развернутое отображение объекта класса Wagon(отображает тип вагона и id) в консоли.
+    def __repr__(self):
+        return f"Тип - {self.wagon_type}(id - {str(id(self))[-3:]})"
+
+    # Возвращает защищенный атрибут __wagon_type (Тип вагона).
+    @property
+    def wagon_type(self):
+        return self.__wagon_type
