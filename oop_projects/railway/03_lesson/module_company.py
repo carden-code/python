32b85a8eb2a19fc895f593bdac6825de0434adc8
Carden <carden.ruby@gmail.com>
2021-02-07 19:02:53 +0300

    modification moduleCompany

--- oop_projects/railway/03_lesson/module_company.py
+++ oop_projects/railway/03_lesson/module_company.py
@@ -3,15 +3,5 @@
 class ModuleCompany:
     # Принимает в виде параметра название компании и сохраняет его.
     def __init__(self):
-        self.__name_company = None
-
-    # Возвращает название компании.
-    @property
-    def name_company(self):
-        return self.__name_company
-
-    # Принимает название компании и записывает его в атрибут.
-    @name_company.setter
-    def name_company(self, name):
-        self.__name_company = name
+        self.name_company = None
 
