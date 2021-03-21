174df63dd1681abf82edbbe3ae854eb31dc8aa0b
Carden <carden.ruby@gmail.com>
2021-03-21 20:57:25 +0300

    modification

--- sravnenie_strok.py
+++ sravnenie_strok.py
@@ -1,13 +1,34 @@
 import re
-a = 'Стефанос Циципас'
-b = 'Циципас, Стефанос'
+from difflib import SequenceMatcher
+
+a = 'К.Кристиэн V С.Сантамария'.lower()
+b = 'Кристиан/ V Сантамарея'.lower()
 
 
-def sravnenie(a, b):
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+
+def sravnenie(string_1, string_2):
+    if '/' in string_1 or '/' in string_2:
+        res = similar(string_1, string_2)
+        print(res)
+        if res > 0.77:
+            return True
+        else:
+            return False
+
     arr_a = re.sub('[/,-]', ' ', a).split()
     arr_b = re.sub('[/,-]', ' ', b).split()
     arr_a.sort(key=lambda x: len(list(x)))
     arr_b.sort(key=lambda x: len(list(x)))
+
+    if len(' '.join(arr_a)) != len(' '.join(arr_b)):
+        res = similar(a, b)
+        if res > 0.8:
+            return True
+        else:
+            return False
 
     result_fast = [x for x in arr_a if x in arr_b]
     if len(result_fast) == len(arr_a):
@@ -15,7 +36,6 @@ def sravnenie(a, b):
 
     result = []
     count_x = 0
-
     for i in range(len(arr_a)):
         slovo1 = arr_a[i]
         slovo2 = arr_b[i]
@@ -24,7 +44,13 @@ def sravnenie(a, b):
         else:
             arr_slovo1 = list(slovo1)
             arr_slovo2 = list(slovo2)
-            for s in range(len(arr_slovo1)):
+
+            if len(arr_slovo1) < len(arr_slovo2):
+                arr_range = len(arr_slovo1)
+            else:
+                arr_range = len(arr_slovo2)
+
+            for s in range(arr_range):
                 if arr_slovo1[s] == arr_slovo2[s]:
                     count_x += 1
             if count_x == len(arr_a) or count_x + 1 == len(arr_a):
