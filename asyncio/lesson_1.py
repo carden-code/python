5f7eaf8f1b20908e45c29e35914ae3a0ba7ba803
Carden <carden.ruby@gmail.com>
2021-04-07 22:41:37 +0300

    add async def, add await asyncio.sleep

--- asyncio/lesson_1.py
+++ asyncio/lesson_1.py
@@ -1,17 +1,31 @@
 from time import sleep, time
+import asyncio
 
 
 start = time()
 
 
-def spider(site_name):
+# Возвращает корутину или сопрограмму.
+async def spider(site_name):
     for page in range(1, 4):
-        sleep(1)
+        # await передаёт управление из функции обратно в event_loop что бы диспетчер мог запустить другие функции.
+        await asyncio.sleep(1)
         print(site_name, page)
 
 
-spider("Blog")
-spider("News")
-spider("Forum")
+spiders = [
+    asyncio.ensure_future(spider("Blog")),
+    asyncio.ensure_future(spider("News")),
+    asyncio.ensure_future(spider('Forum'))
+
+]
+# Диспетчер событий.
+event_loop = asyncio.get_event_loop()
+
+# Сообщаем диспетчеру событий что он должен выполнятся до тех пока не завершатся все сопрограммы(корутины) из списка.
+event_loop.run_until_complete(asyncio.gather(*spiders))
+
+# Завершаем цикл.
+event_loop.close()
 
 print(time() - start)
